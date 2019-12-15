4bd947c89d2b56bb63346dbafe57883e9074c1c7
ceifa <gabrinelson27@gmail.com>
2019-12-15 19:38:27 -0300

    Lazy to type

---
+++ deathrun/build.sh
@@ -0,0 +1,5 @@
+# BUILD IMAGE
+docker build . -t ceifa/lory-gmod-deathrun
+
+# WAIT FOR INPUT, USEFUL TO SEE UNEXPECTED ERRORS
+read
---
+++ deathrun/start.sh
@@ -0,0 +1,23 @@
+NAME="lory-gmod-deathrun"
+
+# CHECK IF CONTAINER ALREADY EXISTS
+if [ "$(docker ps -a -q -f name="${NAME}" --format '{{.Names}}')" == "${NAME}" ]; then
+    # CHECK IF THE CONTAINER IS RUNNING
+    if [ "$(docker ps -q -f name="${NAME}" --format '{{.Names}}')" == "${NAME}" ]; then
+        docker stop "${NAME}" -t 0
+    fi
+
+    docker rm "${NAME}"
+fi
+
+# START CONTAINER
+docker run \
+    -p 27015:27015/udp \
+    -p 27015:27015 \
+    -p 27005:27005/udp \
+    -it \
+    --name "${NAME}" \
+    ceifa/"${NAME}"
+
+# WAIT FOR INPUT, USEFUL TO SEE UNEXPECTED ERRORS
+read
