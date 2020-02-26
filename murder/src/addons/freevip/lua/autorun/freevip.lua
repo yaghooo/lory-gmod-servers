4fea9b6f8d9490336b6a236cd7f12707322986a5
ceifa <gabrinelson27@gmail.com>
2020-02-26 19:37:40 -0300

    free vip

--- murder/src/addons/freevip/lua/autorun/freevip.lua
+++ murder/src/addons/freevip/lua/autorun/freevip.lua
@@ -1,7 +1,9 @@
 if SERVER then
     hook.Add("PlayerInitialSpawn", "freevip", function(ply)
-        local userInfo = ULib.ucl.authed[ply:UniqueID()]
-        ULib.ucl.addUser(ply:SteamID(), userInfo.allow, userInfo.deny, "vip")
-        ulx.CreateExpiration(ply, 14400, "user")
+        if ply:GetUserGroup() == "user" then
+            local userInfo = ULib.ucl.authed[ply:UniqueID()]
+            ULib.ucl.addUser(ply:SteamID(), userInfo.allow, userInfo.deny, "vip")
+            ulx.CreateExpiration(ply, 14400, "user")
+        end
     end)
 end
