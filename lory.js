ba6cbd8d3bb817f2ea4dd28e977e3afd793a551d
ceifa <gabrinelson27@gmail.com>
2020-03-07 18:27:41 -0300

    changes

--- lory.js
+++ lory.js
@@ -2,84 +2,92 @@ const { promises: fs } = require("fs");
 const path = require("path");
 
 function setProduction(v) {
-    process.env['PRODUCTION'] = Number(v);
+	process.env["PRODUCTION"] = Number(v);
 }
 
 async function mkdirOrIgnore(dir) {
-    try {
-        await fs.mkdir(dir);
-    } catch (e) {
-        if (e.errno !== -4075) {
-            throw e;
-        }
-    }
+	try {
+		await fs.mkdir(dir);
+	} catch (e) {}
 }
 
 async function mergeDirs(src, dest) {
-    const files = await fs.readdir(src);
+	const files = await fs.readdir(src);
 
-    for (const file of files) {        
-        const srcFile = path.resolve(src, file);
-        const destFile = path.resolve(dest, file);
-        
-        const stats = await fs.lstat(srcFile);        
-        if (stats.isDirectory()) {
-            await mkdirOrIgnore(destFile);
-            await mergeDirs(srcFile, destFile);
-        } else {
-            await fs.writeFile(destFile, await fs.readFile(srcFile));
-        }
-    }
+	for (const file of files) {
+		const srcFile = path.resolve(src, file);
+		const destFile = path.resolve(dest, file);
+
+		const stats = await fs.lstat(srcFile);
+		if (stats.isDirectory()) {
+			await mkdirOrIgnore(destFile);
+			await mergeDirs(srcFile, destFile);
+		} else {
+			await fs.writeFile(destFile, await fs.readFile(srcFile));
+		}
+	}
 }
 
 async function build([servers, env]) {
-    const serversDir = path.resolve(__dirname, "servers");
+	const serversDir = path.resolve(__dirname, "servers");
 
-    let serversToBuild = [];
-    if (!servers || servers === "*") {
-        const serversDirs = await fs.readdir(serversDir);
-        serversToBuild = serversDirs.filter(s => !s.startsWith("_"));
-    } else {
-        serversToBuild = servers.split(",");
-    }
+	let serversToBuild = [];
+	if (!servers || servers === "*") {
+		const serversDirs = await fs.readdir(serversDir);
+		serversToBuild = serversDirs.filter(s => !s.startsWith("_"));
+	} else {
+		serversToBuild = servers.split(",");
+	}
 
-    const buildDir = path.resolve(serversDir, "_build");
-    await mkdirOrIgnore(buildDir);
+	env = env || "prod";
+	console.log(`Building servers on ${env} mode`);
 
-    for (const serverName of serversToBuild) {
-        const serverBuildDir = path.resolve(buildDir, serverName);
-        await mkdirOrIgnore(serverBuildDir);
+	const buildDir = path.resolve(serversDir, "_build");
+	await mkdirOrIgnore(buildDir);
 
-        const sharedDir = path.resolve(serversDir, "_shared");
-        await mergeDirs(sharedDir, serverBuildDir);
+	const stateDir = path.resolve(serversDir, "_stateful");
+	await mkdirOrIgnore(stateDir);
 
-        const isDev = env === "dev";
-        if (isDev) {
-            const debugDir = path.resolve(serversDir, "_debug");
-            await mergeDirs(debugDir, serverBuildDir);
-        }
-    
-        setProduction(!isDev);
+	for (const serverName of serversToBuild) {
+		const serverBuildDir = path.resolve(buildDir, serverName);
+		await mkdirOrIgnore(serverBuildDir);
 
-        await mergeDirs(path.resolve(serversDir, serverName), serverBuildDir);
-    }
+		const serverStateDir = path.resolve(stateDir, serverName);
+		await mkdirOrIgnore(serverStateDir);
+		await fs.writeFile(path.resolve(serverStateDir, "sv.db"), "");
+		await fs.writeFile(path.resolve(serverStateDir, "debug.log"), "");
+
+		const sharedDir = path.resolve(serversDir, "_shared");
+		await mergeDirs(sharedDir, serverBuildDir);
+
+		if (env === "dev") {
+			const debugDir = path.resolve(serversDir, "_debug");
+			await mergeDirs(debugDir, serverBuildDir);
+			setProduction(false);
+		} else {
+			setProduction(true);
+		}
+
+		await mergeDirs(path.resolve(serversDir, serverName), serverBuildDir);
+		console.log(`Successfully built server ${serverName}`);
+	}
 }
 
 async function start([servers, env]) {
-    await build([servers, env || "dev"]);
+	await build([servers, env || "dev"]);
 }
 
 const actions = {
-    start,
-    build
+	start,
+	build
 };
 
 const [chosenAction, ...actionArgs] = process.argv.slice(2);
 
 (async () => {
-    if (chosenAction in actions) {
-        await actions[chosenAction](actionArgs);
-    } else {
-        console.error(`Action "${chosenAction}" not found`);
-    }
+	if (chosenAction in actions) {
+		await actions[chosenAction](actionArgs);
+	} else {
+		console.error(`Action "${chosenAction}" not found`);
+	}
 })();
