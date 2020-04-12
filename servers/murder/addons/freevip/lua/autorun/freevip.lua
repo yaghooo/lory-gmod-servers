2b70c049327c5346c81682df08ca187a940a1e0a
ceifa <gabrinelson27@gmail.com>
2020-04-12 20:45:26 -0300

    Remove free vip

--- servers/murder/addons/freevip/lua/autorun/freevip.lua
+++
@@ -1,9 +0,0 @@
-if SERVER then
-    hook.Add("PlayerInitialSpawn", "freevip", function(ply)
-        if ply:GetUserGroup() == "user" then
-            local userInfo = ULib.ucl.authed[ply:UniqueID()]
-            ULib.ucl.addUser(ply:SteamID(), userInfo.allow, userInfo.deny, "vip")
-            ulx.CreateExpiration(ply, 14400, "user")
-        end
-    end)
-end
