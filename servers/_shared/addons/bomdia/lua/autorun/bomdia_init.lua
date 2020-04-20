febf2e4910001eafdfccaffec43ced31e5f3d5bf
Gabriel Francisco <gabrinelson27@gmail.com>
2020-04-19 22:11:05 -0300

    Update bomdia_init.lua

--- servers/_shared/addons/bomdia/lua/autorun/bomdia_init.lua
+++ servers/_shared/addons/bomdia/lua/autorun/bomdia_init.lua
@@ -3,9 +3,8 @@ AddCSLuaFile("bomdia/cl_bomdia.lua")
 if CLIENT then
     include("bomdia/cl_bomdia.lua")
 else
+    CreateConVar("bomdia_cabuloso_rate", 0.85, FCVAR_NONE, "Rate of bom dia cabuloso")
+    CreateConVar("bomdia_cabuloso_points", 200, FCVAR_NONE, "Points that a bom dia cabuloso should give")
+    CreateConVar("bomdia_interval_time", 300, FCVAR_NONE, "Interval between bom dias")
     include("bomdia/sv_bomdia.lua")
 end
-
-CreateConVar("bomdia_cabuloso_rate", 0.85, FCVAR_NONE, "Rate of bom dia cabuloso")
-CreateConVar("bomdia_cabuloso_points", 200, FCVAR_NONE, "Points that a bom dia cabuloso should give")
-CreateConVar("bomdia_interval_time", 300, FCVAR_NONE, "Interval between bom dias")
