0be03beb764a878a14b923da4e8e08cf078661ba
ceifa <gabrinelson27@gmail.com>
2020-04-22 15:09:07 -0300

    Register needed

--- servers/_shared/addons/utils/lua/autorun/server/discord.lua
+++ servers/_shared/addons/utils/lua/autorun/server/discord.lua
@@ -61,3 +61,11 @@ hook.Add("PlayerSay", "DiscordRegister", function(ply, text)
         end)
     end
 end)
+
+-- REMOVE LATER
+hook.Add("PlayerAuthed", "KickNonRegistered", function(ply, steamId)
+    local sid64 = IsValid(ply) and ply:SteamID64() or util.SteamIDTo64(steamId)
+    if not DISCORD:IsRegistered(sid64) then
+        ply:Kick("Servidor privado para usuários registrados")
+    end
+end)
