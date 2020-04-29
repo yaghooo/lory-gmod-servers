8400af871a5a6d209d02d19f09c6dc527cc977a0
ceifa <gabrinelson27@gmail.com>
2020-04-29 17:04:31 -0300

    afk check

---
+++ servers/_shared/addons/utils/lua/autorun/server/antiafk.lua
@@ -0,0 +1,17 @@
+local AFKLimit = CreateConVar("antiafk_limit", 60 * 5, FCVAR_NONE, "Max time in seconds player can stay AFK before being kicked", 20)
+
+hook.Add("PlayerChangedTeam", "CheckAFKPlayer", function(ply)
+    local identifier = "AFKCheck" .. ply:SteamID()
+
+    if ply.Spectating then
+        timer.Create(identifier, AFKLimit:GetInt() - 10, 1, function()
+            ply:ChatPrint("Você será kickado em 10 segundos se continuar de espectador")
+
+            timer.Create(identifier, 10, 1, function()
+                ply:Kick("Ficou muito tempo como um espectador.")
+            end)
+        end)
+    else
+        timer.Remove(identifier)
+    end
+end)
