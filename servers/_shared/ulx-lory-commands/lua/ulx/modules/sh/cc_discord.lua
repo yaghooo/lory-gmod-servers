0389d7fe278d60a1c4682c9b8e1aa26eccce37e3
ceifa <gabrinelson27@gmail.com>
2020-05-05 11:13:38 -0300

    Fix discord command

--- servers/_shared/ulx-lory-commands/lua/ulx/modules/sh/cc_discord.lua
+++ servers/_shared/ulx-lory-commands/lua/ulx/modules/sh/cc_discord.lua
@@ -2,34 +2,36 @@ local CATEGORY_NAME = "Lory"
 
 local discordrank = ulx.command(CATEGORY_NAME, "ulx discordrank", function(ply, discordId, group)
     group = group:lower()
-    local sid64 = DISCORD:GetSid64ById(discord_id)
-    print("Add user to rank " .. group .. " with discord '" .. discord_id .. "' and id '" .. sid64 .. "'")
+    local sid64 = DISCORD:GetSid64ById(discordId)
+    print("Add user to rank " .. group .. " with discord '" .. discordId .. "' and id '" .. sid64 .. "'")
     ULib.ucl.addUser(util.SteamIDFrom64(sid64), userInfo.allow, userInfo.deny, group)
 end)
 
 discordrank:addParam{
-    type = ULib.cmds.StringArg
+    type = ULib.cmds.StringArg,
+    hint = "Discord id"
 }
 
 discordrank:addParam{
     type = ULib.cmds.StringArg,
     completes = ulx.tempuser_group_names,
-    hint = "Group to place user in temporarily",
+    hint = "Group to place user",
     error = "invalid group '%s' specified",
-ULib.cmds.restrictToCompletes
+    ULib.cmds.restrictToCompletes
 }
 
 discordrank:defaultAccess(ULib.ACCESS_ALL)
 discordrank:help("Adiciona rank pelo id do discord.")
 
 local discordpoints = ulx.command(CATEGORY_NAME, "ulx discordpoints", function(ply, discordId, quantity)
-    local sid64 = DISCORD:GetSid64ById(discord_id)
-    print("Add " .. quantity .. " points to user with discord '" .. discord_id .. "' and id '" .. sid64 .. "'")
+    local sid64 = DISCORD:GetSid64ById(discordId)
+    print("Add " .. quantity .. " points to user with discord '" .. discordId .. "' and id '" .. sid64 .. "'")
     PS.DataProvider:GivePoints(sid64, quantity)
 end)
 
 discordpoints:addParam{
-    type = ULib.cmds.StringArg
+    type = ULib.cmds.StringArg,
+    hint = "Discord id"
 }
 
 discordpoints:addParam{
