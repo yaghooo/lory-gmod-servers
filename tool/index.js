309c040e588e9e6b6a8e5d1ce638a811934d66f1
ceifa <gabrinelson27@gmail.com>
2020-05-08 18:22:03 -0300

    Tool can be used from any path

--- tool/index.js
+++ tool/index.js
@@ -9,22 +9,24 @@ var copy = require('recursive-copy')
   const isDev = options.some((s) => s === '--dev')
   const servers = options.filter((s) => !s.startsWith('--'))
 
-  await del('../build', { force: true })
+  await del(path.resolve(__dirname, '../build'), { force: true })
 
   for (const server of servers) {
     const paths = [
       ...(scheme.production[server].paths || []),
-      ...(isDev && scheme.developing[server].paths || []),
-    ].map(p => path.join('../servers', p).replace(/\\/g, '/'))
+      ...((isDev && scheme.developing[server].paths) || []),
+    ].map((p) => path.resolve(__dirname, '../servers', p).replace(/\\/g, '/'))
 
     const filters = [
-      "**/*",
+      '**/*',
       ...(scheme.production[server].filters || []),
-      ...(isDev && scheme.developing[server].filters || []),
+      ...((isDev && scheme.developing[server].filters) || []),
     ]
 
-    for (const relativePath of paths) {      
-      await copy(relativePath, path.join('../build/', server), { filter: filters })
+    for (const relativePath of paths) {
+      await copy(relativePath, path.resolve(__dirname, '../build/', server), {
+        filter: filters,
+      })
     }
 
     console.log(`Build for ${server} succeed`)
