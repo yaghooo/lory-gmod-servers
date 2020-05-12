310a60ca3581bbf8f4b0f843dc91733d9825b06b
ceifa <gabrinelson27@gmail.com>
2020-05-12 13:09:43 -0300

    idk

--- tool/build.sh
+++ tool/build.sh
@@ -1,2 +1,2 @@
-cargo build --release --out-dir ../bin -Z unstable-options
-cmd.exe /k "cargo build --release --out-dir ../bin -Z unstable-options && exit"
+cargo build --release --all-features --bins --out-dir ../bin -Z unstable-options
+cmd.exe /k "cargo build --release --all-features --bins --out-dir ../bin -Z unstable-options && exit"
