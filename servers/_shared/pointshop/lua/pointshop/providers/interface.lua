967e5de2ca00c7c87b68e479cab6ae3c440c3656
ceifa <gabrinelson27@gmail.com>
2020-05-22 15:08:53 -0300

    Fix mysql configurations

--- servers/_shared/pointshop/lua/pointshop/providers/interface.lua
+++ servers/_shared/pointshop/lua/pointshop/providers/interface.lua
@@ -1,24 +1,29 @@
 require("mysqloo")
-
 mysql = {}
-
 local mysql_hostname = CreateConVar("mysql_hostname", "localhost", FCVAR_NONE, "MySql hostname")
 local mysql_username = CreateConVar("mysql_username", "root", FCVAR_NONE, "MySql Username")
 local mysql_password = CreateConVar("mysql_password", "", FCVAR_NONE, "MySql Password")
 local mysql_database = CreateConVar("mysql_database", "lory", FCVAR_NONE, "MySql Database")
 local mysql_port = 3306 -- Your MySQL port. Most likely is 3306.
+local db
 
-local db = mysqloo.connect(mysql_hostname:GetString(), mysql_username:GetString(), mysql_password:GetString(), mysql_database:GetString(), mysql_port)
+-- Ensure the connection will be made after loading CFG files
+timer.Simple(0, function()
+    local connection = {mysql_hostname:GetString(), mysql_username:GetString(), mysql_password:GetString(), mysql_database:GetString(), mysql_port}
+    print("MySQL Credentials:")
+    PrintTable(connection)
+    db = mysqloo.connect(unpack(connection))
 
-function db:onConnected()
-    MsgC("MySQL: Connected!")
-end
+    function db:onConnected()
+        MsgC("MySQL: Connected!")
+    end
 
-function db:onConnectionFailed(err)
-    MsgC(Color(255, 0, 0), "MySQL: Connection Failed, please check your settings: ", err)
-end
+    function db:onConnectionFailed(err)
+        MsgC(Color(255, 0, 0), "MySQL: Connection Failed, please check your settings: ", err)
+    end
 
-db:connect()
+    db:connect()
+end)
 
 function mysql.Query(query, callback)
     local q = db:query(query)
