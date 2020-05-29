e1ed87887532f52508269dcc79e3970f59b8f072
Natanfc100 <32806615+Natanfc100@users.noreply.github.com>
2020-05-29 12:42:35 -0300

    aumentei o preço de gyakushinn

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/gyakushinn_miko.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/gyakushinn_miko.lua
@@ -1,4 +1,4 @@
 ITEM.Name = "Gyakushinn Miko"
-ITEM.Price = 25000
+ITEM.Price = 40000
 ITEM.Model = "models/pacagma/houkai_impact_3rd/gyakushinn_miko/gyakushinn_miko_player.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
