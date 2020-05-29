27a69e608d1d6d80d16924a18fb9cbfc37855770
Gabriel Francisco <gabrinelson27@gmail.com>
2020-05-29 16:33:36 -0300

    Update fatman.lua

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/fatman.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/fatman.lua
@@ -2,4 +2,3 @@ ITEM.Name = "Gordo Brony"
 ITEM.Price = 10000
 ITEM.Model = "models/dawson/obese_male_deluxe/obese_male_deluxe.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
-ITEM.Bodygroup = "10000032"
