287d8785d091ae33b66e88a581fb474d74dba9df
Natanfc100 <32806615+Natanfc100@users.noreply.github.com>
2020-05-29 12:45:42 -0300

    aumentei o preço de kokichi oma

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/kokich_oma.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/kokich_oma.lua
@@ -1,4 +1,4 @@
 ITEM.Name = "Kokichi Oma"
-ITEM.Price = 10000
+ITEM.Price = 50000
 ITEM.Model = "models/player_kokichioumaschool.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
