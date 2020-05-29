fd5b7f5ca48d5ae37eeda980074576bd070f42ca
Natanfc100 <32806615+Natanfc100@users.noreply.github.com>
2020-05-29 10:42:36 -0300

    aumentei o preço do batman

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/batman.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/batman.lua
@@ -1,4 +1,4 @@
 ITEM.Name = "Batman"
-ITEM.Price = 10000
+ITEM.Price = 20000
 ITEM.Model = "models/player/pizzaroll/akdemonbatman.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
