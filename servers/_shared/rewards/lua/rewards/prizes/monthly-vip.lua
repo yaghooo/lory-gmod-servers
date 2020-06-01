6a64f2be0c9d2090d0680b6f03a7e69e45c7bfd3
Gabriel Francisco <gabrinelson27@gmail.com>
2020-06-01 19:29:46 -0300

    Increase vip points earned

--- servers/_shared/rewards/lua/rewards/prizes/monthly-vip.lua
+++ servers/_shared/rewards/lua/rewards/prizes/monthly-vip.lua
@@ -1,7 +1,7 @@
 PRIZE.Title = "VIP MENSAL"
 PRIZE.Description = "Ganha 50.000 " .. PS.Config.PointsName
 PRIZE.Image = "star"
-PRIZE.Points = 50000
+PRIZE.Points = 60000
 
 function PRIZE:GetStatus(ply)
     if not ply:IsUserGroup("vip") then
