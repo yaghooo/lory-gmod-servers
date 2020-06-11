30d53dc766798ff91c5277cb1b308be05fad8574
ceifa <gabrinelson27@gmail.com>
2020-06-11 13:29:38 -0300

    Push refactoring

--- servers/_shared/push/lua/autorun/server/push.lua
+++ servers/_shared/push/lua/autorun/server/push.lua
@@ -13,41 +13,47 @@ local possible_sounds = {
 
 local pushs = {}
 
-function PushEntity(originator, ent)
+function PushEntity(originator, ent, originatorId)
     originator:EmitSound(table.Random(possible_sounds), 100, math.random(90, 110))
     ent:SetVelocity(originator:EyeAngles():Forward() * push_force:GetInt())
     ent:ViewPunch(Angle(math.random(-20, 20), math.random(-20, 20), 0))
 
-    pushs[originator:UserID()] = true
+    pushs[originatorId] = true
     timer.Simple(0.3, function()
-        pushs[originator:UserID()] = false
+        pushs[originatorId] = nil
     end)
 end
 
 hook.Add("KeyPress", "Pushing", function(ply, key)
-    if key == IN_USE and not pushs[ply:UserID()] then
+    if key == IN_USE then
         if GhostMode and ply:IsGhost() then
             return
         end
 
-        local playerHasPush = PS and ply:PS_HasItemEquipped("push")
-        local playerHasAntiPush = PS and ply:PS_HasItemEquipped("antipush")
+        local originatorId = ply:SteamID()
+        if pushs[originatorId] then
+            return
+        end
 
-        if playerHasPush and not playerHasAntiPush then
-            local ent = ply:GetEyeTrace().Entity
+        local ent = ply:GetEyeTrace().Entity
 
-            if IsValid(ply) and IsValid(ent) and ply:IsPlayer() and ent:IsPlayer() and ent:Alive() then
-                local isEntityClose = ply:GetPos():Distance(ent:GetPos()) <= 200
-                local entMoveType = ent:GetMoveType()
-                local isMoveTypeValid = entMoveType ~= MOVETYPE_OBSERVER and entMoveType ~= MOVETYPE_LADDER
+        if PS then
+            local playerHasPush = ply:PS_HasItemEquipped("push")
+            local playerHasAntiPush = ply:PS_HasItemEquipped("antipush")
+            local entityHasAntiPush = ent:IsPlayer() and ent:PS_HasItemEquipped("antipush")
 
-                if isEntityClose and isMoveTypeValid then
-                    local entityHasAntiPush = PS and ent:PS_HasItemEquipped("antipush")
+            if not playerHasPush or playerHasAntiPush or entityHasAntiPush then
+                return
+            end
+        end
 
-                    if not entityHasAntiPush then
-                        PushEntity(ply, ent)
-                    end
-                end
+        if IsValid(ply) and IsValid(ent) and ply:IsPlayer() and ent:IsPlayer() and ent:Alive() then
+            local isEntityClose = ply:GetPos():Distance(ent:GetPos()) <= 200
+            local entMoveType = ent:GetMoveType()
+            local isMoveTypeValid = entMoveType ~= MOVETYPE_OBSERVER and entMoveType ~= MOVETYPE_LADDER
+
+            if isEntityClose and isMoveTypeValid then
+                PushEntity(ply, ent, originatorId)
             end
         end
     end
