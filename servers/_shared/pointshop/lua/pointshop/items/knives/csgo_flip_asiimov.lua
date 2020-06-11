5b5d7e7c0abbe3910d70ca5b337c695599d96f7b
ceifa <gabrinelson27@gmail.com>
2020-06-11 16:18:05 -0300

    Add flip asiimov to test

---
+++ servers/_shared/pointshop/lua/pointshop/items/knives/csgo_flip_asiimov.lua
@@ -0,0 +1,5 @@
+ITEM.Name = "Flip" .. " | " .. "Asiimov"
+ITEM.Price = 169500
+ITEM.Model = "models/weapons/w_csgo_flip.mdl"
+ITEM.WeaponClass = "csgo_flip_asiimov"
+ITEM.PaintMaterial = "models/csgo_knife/flipknife_asiimov"
