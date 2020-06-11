95cee9298a6c61c418ab4ea35d542ee81a492585
ceifa <gabrinelson27@gmail.com>
2020-06-11 16:48:24 -0300

    Remove asiimov

--- servers/_shared/pointshop/lua/pointshop/items/knives/csgo_flip_asiimov.lua
+++
@@ -1,5 +0,0 @@
-ITEM.Name = "Flip" .. " | " .. "Asiimov"
-ITEM.Price = 169500
-ITEM.Model = "models/weapons/w_csgo_flip.mdl"
-ITEM.WeaponClass = "csgo_flip_asiimov"
-ITEM.PaintMaterial = "models/csgo_knife/flipknife_asiimov"
