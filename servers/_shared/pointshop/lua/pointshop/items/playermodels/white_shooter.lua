ac66c75299ab95d0d21351e5017c37ee5bd582b4
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-12 17:44:26 -0300

    Adicionei genero a White Shooter, female

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/white_shooter.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/white_shooter.lua
@@ -2,3 +2,4 @@ ITEM.Name = "White Shooter"
 ITEM.Price = 20000
 ITEM.Model = "models/jazzmcfly/wrs/wrs.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
+ITEM.Sex = "female"
