cc7cdaf5ddf6000fe29d3f4e190dc18509960678
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-12 15:40:54 -0300

    Adicionei genero a Remilia Scarlet, female

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/remilia.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/remilia.lua
@@ -2,3 +2,4 @@ ITEM.Name = "Remilia Scarlet"
 ITEM.Price = 35000
 ITEM.Model = "models/loyalists/mmd/remilia/remilia_mp_pm.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
+ITEM.Sex = "female"
