df6238f1c16e084f819866d5eb1be2e9a24f4984
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-12 19:18:35 -0300

    Adicionei genero ao Gordo Brony, male

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/fatman.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/fatman.lua
@@ -2,3 +2,4 @@ ITEM.Name = "Gordo Brony"
 ITEM.Price = 15000
 ITEM.Model = "models/dawson/obese_male_deluxe/obese_male_deluxe.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
+ITEM.Sex = "male"
