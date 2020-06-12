04f2daddcb44ec1f0c53a3586c96c393be4e2d22
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-12 20:51:23 -0300

    adicionei genero ao batman, male (#162)

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/batman.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/batman.lua
@@ -2,3 +2,4 @@ ITEM.Name = "Batman"
 ITEM.Price = 20000
 ITEM.Model = "models/player/pizzaroll/akdemonbatman.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
+ITEM.Sex = "male"
