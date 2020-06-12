3c2a5835c5cc7c35fc11f097b2014cc8bba924b8
tiobacca <66842904+tiobacca@users.noreply.github.com>
2020-06-12 15:18:27 -0300

    adicionei genero a Lucoa, female

--- servers/_shared/pointshop/lua/pointshop/items/playermodels/lucoa.lua
+++ servers/_shared/pointshop/lua/pointshop/items/playermodels/lucoa.lua
@@ -2,3 +2,4 @@ ITEM.Name = "Lucoa"
 ITEM.Price = 35000
 ITEM.Model = "models/player/dewobedil/maid_dragon/lucoa/default_p.mdl"
 ITEM.AllowedUserGroups = {"superadmin", "admin", "vip", "operator", "contributor"}
+ITEM.Sex = "female"
