ecc192956f3e69aa9292ca418824135d6ba3be42
ceifa <gabrinelson27@gmail.com>
2020-06-16 17:57:06 -0300

    Decrease earn judeus by time

--- servers/_shared/pointshop/lua/pointshop/sh_config.lua
+++ servers/_shared/pointshop/lua/pointshop/sh_config.lua
@@ -10,7 +10,7 @@ PS.Config.LootChance = 0.03
 PS.Config.CrateItemQuantity = 200
 PS.Config.PointsOverTime = true -- Should players be given points over time?
 PS.Config.PointsOverTimeDelay = 5 -- If so, how many minutes apart?
-PS.Config.PointsOverTimeAmount = 200 -- And if so, how many points to give after the time?
+PS.Config.PointsOverTimeAmount = 150 -- And if so, how many points to give after the time?
 PS.Config.AdminCanAccessAdminTab = false -- Can Admins access the Admin tab?
 PS.Config.CanPlayersGiveItems = true
 PS.Config.CanPlayersGivePoints = true -- Can players give points away to other players?
@@ -18,7 +18,7 @@ PS.Config.IsMarketplaceEnabled = true
 PS.Config.PointsName = "Judeu" -- What are the points called?
 PS.Config.SortItemsBy = "Name" -- How are items sorted? Set to 'Price' to sort by price.
 PS.Config.UseMySql = false
-PS.Config.MarketplaceOverTimeHours = 24 * 5
+PS.Config.MarketplaceOverTimeDays = 5
 
 -- Edit below if you know what you're doing
 PS.Config.CalculateBuyPrice = function(ply, item)
